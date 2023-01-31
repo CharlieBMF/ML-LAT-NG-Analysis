2cd84cada64cde8432ecfacd18e6c83dd14c0ae8
CharlieBMF <marcinski.karol@gmail.com>
2023-01-31 14:53:50 +0100

    31-01

--- data/data_main.py
+++ data/data_main.py
@@ -19,23 +19,21 @@ for i in range(len(workbook_main)):
     try:
         if str(workbook_main.iloc[i]['nr lotu']+'.xlsx') != actual_workbook_path:
             actual_workbook_path = str(workbook_main.iloc[i]['nr lotu']+'.xlsx')
-            workbook_temp = pd.read_excel(str(workbook_main.iloc[i]['nr lotu']+'.xlsx'),
+            workbook_temp = pd.read_excel(actual_workbook_path,
                                           engine='openpyxl',
                                           sheet_name='All',
                                           header=4,
                                           usecols="D, I:Q, Z:AQ, AX:AZ, BK:CE, CO:DK, DT:DW, EE:EF, EM:EW, EZ:FC, FF, FH:FK, FN")
-        else:
-            ds = workbook_temp[workbook_temp['Barcode'] == workbook_main.iloc[i]['nr seryjny']]
+
+        ds = workbook_temp[workbook_temp['Barcode'] == workbook_main.iloc[i]['nr seryjny']]
         final = final.append(ds, ignore_index=True)
         print('Finished step..', len(final))
         print('Skipped buddy counter..', skipped_buddy_counter)
-        end = time.time()
-        print('This loop took.. ', end - start)
     except:
         final = final.append(empty, ignore_index=True)
         skipped_buddy_counter += 1
     finally:
-        if i % 10 == 0 or i == len(workbook_main):
-            with pd.ExcelWriter('combined.xlsx') as writer:
-                final.to_excel(writer)
-
+        with pd.ExcelWriter('combined.xlsx') as writer:
+            final.to_excel(writer)
+        end = time.time()
+        print('This loop took.. ', end - start)
